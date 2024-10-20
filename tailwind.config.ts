988d57ad0bb4e5bc23060bf010c430c9e8b95321
jozko25 <janko.tank.poi@gmail.com>
2024-10-20 17:11:54 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,14 @@ const config = {
 	],
   prefix: "",
   theme: {
-  	container: {
-  		center: 'true',
-  		padding: '2rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
-  	},
+	container: {
+		center: true, // Change 'true' to true (no quotes)
+		padding: '2rem',
+		screens: {
+		  '2xl': '1400px',
+		},
+	  },
+	  
   	extend: {
   		colors: {
   			border: 'hsl(var(--border))',
